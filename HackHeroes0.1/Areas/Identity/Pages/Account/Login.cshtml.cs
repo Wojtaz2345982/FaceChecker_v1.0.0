693932082a5565b5df003e8860a9a6ee1f787ac1
Wojciech Błaszczyk <128930219+Wojtaz2345982@users.noreply.github.com>
2023-11-07 17:58:28 +0100

    Update Login.cshtml.cs

--- HackHeroes0.1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HackHeroes0.1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace HackHeroes0._1.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/FaceChecker/Index");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +103,7 @@ namespace HackHeroes0._1.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/FaceChecker/Index");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
